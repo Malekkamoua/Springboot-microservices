3b89a0effac99d9fbac6f71920b65533db4fc018
Malekkamoua <malekkamoua50@gmail.com>
2021-10-14 17:30:48 +0100

    Created 2 services + eureka server
    working fine

---
+++ user_service/src/main/java/com/microservices/user_service/VO/ResponseTemplateVO.java
@@ -0,0 +1,15 @@
+package com.microservices.user_service.VO;
+
+import com.microservices.user_service.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+
+    private User user;
+    private Department department;
+}
